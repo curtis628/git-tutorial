ff1d2f3629fbc562c5dfe10faee714aae1e584bf
Tyler Curtis <tjcurt@gmail.com>
2026-03-20 13:25:20 -0700

    Initial commit of bare-bones java app

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # git-tutorial
 Interactive git tutorial
+
+Setup
+-----
+1. Create bare-bones Java project via `mvn:archetype`
+```bash
+mvn archetype:generate \
+  -DgroupId=net.tcurt.tutorials.git \
+  -DartifactId=git-tutorial \
+  -DarchetypeArtifactId=maven-archetype-quickstart \
+  -DarchetypeVersion=1.4 \
+  -DinteractiveMode=false
+```
---
+++ src/main/java/net/tcurt/tutorials/git/App.java
@@ -0,0 +1,19 @@
+package net.tcurt.tutorials.git;
+
+/** Hello world! */
+public class App {
+  private final String defaultName;
+
+  public App(String defaultName) {
+    this.defaultName = defaultName;
+  }
+
+  public void display() {
+    System.out.println("Hello, " + defaultName);
+  }
+
+  public static void main(String[] args) {
+    App app = new App("Git");
+    app.display();
+  }
+}
